06187a91ff0a27a612adbb3341e9f7b362db4fc2
kif-mohammed <85088553+kif-mohammed@users.noreply.github.com>
2021-06-10 18:15:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+'''
 from flask import Flask, request, make_response
+'''
+
+from flask import Flask,request,jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello again, Universal!"
+
+@app.route('/testme')
+def home():
+    return 'success'
+'''
+@app.route('/simulate',methods=['POST'])
+def recommend():
+    df_result=get_recommendation(request)
+    return(jsonify(df_result.to_json(orient='records')))
+#     return recommendation(request)
+if __name__=='__main__':
+    app.run(debug=True)
+
+'''
